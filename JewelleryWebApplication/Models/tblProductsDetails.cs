fb537d5bcd0110f940e530233b925787ad2e3d77
vishwaspalve93 <vishwas.palave@loyalstring.com>
2023-07-21 07:26:09 +0530

    datatype changes

--- JewelleryWebApplication/Models/tblProductsDetails.cs
+++ JewelleryWebApplication/Models/tblProductsDetails.cs
@@ -4,7 +4,7 @@ namespace JewelleryWebApplication.Models
 {
     public class tblProductsDetails : BaseModel
     {
-        public string createdDate { get; set; }
+      
         public string createdBy { get; set; }
         public string tidValue { get; set; }
         public string epcValue { get; set; }
@@ -14,9 +14,9 @@ namespace JewelleryWebApplication.Models
         public string barcodeNumber { get; set; }
         public string itemCode { get; set; }
         public string box { get; set; }
-        public double grossWeight { get; set; }
-        public double netWeight { get; set; }
-        public double stoneweight { get; set; }
+        public float grossWeight { get; set; }
+        public float netWeight { get; set; }
+        public float stoneweight { get; set; }
         public string makinggm { get; set; }
         public string makingper { get; set; }
         public string fixedamount { get; set; }
